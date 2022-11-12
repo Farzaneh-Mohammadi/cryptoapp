5b71e6d6a40ac6348c1d5b53549a819f2a315bfc
Farzaneh-Mohammadi <mohammadifh8931@gmail.com>
2022-11-12 15:54:44 +0330

    crypto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="app">
+        <Header />
+        <Routes>
+
+        <Route path="/" element={<Homepage />}  />
+        <Route path="/coins/:id" element={<CoinPage />}  />
+        </Routes>
+
+      </div>
+
   );
 }
 
---
+++ src/components/SelectButton.js
@@ -0,0 +1,12 @@
+import "./SelectButton.css"
+const SelectButton = ({ children, selected, onClick }) => {
+
+
+  return (
+    <span onClick={onClick} className="selectbutton">
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
